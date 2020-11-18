0c10231d554203d8cacb7a3411999b0e30144de4
Mohammed Akel <akel.ms93@gmail.com>
2020-11-18 01:23:43 -0500

    Toogle charts elements

--- src/Components/Charts/GlobalDaily/GlobalDaily.js
+++ src/Components/Charts/GlobalDaily/GlobalDaily.js
@@ -1,17 +1,54 @@
 import React, {useEffect, useState} from 'react'
+import { withStyles } from '@material-ui/core/styles';
 import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip} from 'recharts';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const ConfirmedCheckbox = withStyles({
+  root: {
+    color: '#5078AA',
+    '&$checked': {
+      color: '#5078AA',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+const RecoveredCheckbox = withStyles({
+  root: {
+    color: '#74FFA7',
+    '&$checked': {
+      color: '#74FFA7',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+
+const DeathsCheckBox = withStyles({
+  root: {
+    color: '#FF6060',
+    '&$checked': {
+      color: '#FF6060',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 const GlobalDaily = ({data}) => {
   const [dailyData, setDailyData] = useState()
+  const [displayConfirmed, setDisplayConfirmed] = useState(true)
+  const [displayRecovered, setDisplayRecovered] = useState(true)
+  const [displayDeaths, setDisplayDeaths] = useState(true)
 
   const CustomTooltip = ({ payload }) => {
     return (
       <div className="tooltip">
         <b>{payload?.[0]?.payload?.date}</b>
-        <span >
-          <p >
-            Confirmed: {payload?.[0]?.payload?.confirmed.toLocaleString()}
-          </p>
+        <span>
+          {displayConfirmed && <p>Confirmed: {payload?.[0]?.payload?.confirmed.toLocaleString()}</p>}
+          {displayRecovered && <p>Recovered: {payload?.[0]?.payload?.recovered.toLocaleString()}</p>}
+          {displayDeaths && <p>Deaths: {payload?.[0]?.payload?.deaths.toLocaleString()}</p>}
         </span>
       </div>
     )
@@ -20,11 +57,24 @@ const GlobalDaily = ({data}) => {
   useEffect(() => {
     if(data) {
       let dataCopy = JSON.parse(JSON.stringify(data))
-      let last = 0
+      let lastConfirmed = 0
+      let lastRecovered = 0
+      let lastDeaths = 0
       for(let day in dataCopy) {
-        let current = JSON.parse(JSON.stringify(dataCopy[day].confirmed)) 
-        dataCopy[day].confirmed = current - last
-        last = current
+        //Confirmed
+        let currentConfirmed = JSON.parse(JSON.stringify(dataCopy[day].confirmed)) 
+        dataCopy[day].confirmed = currentConfirmed - lastConfirmed
+        lastConfirmed = currentConfirmed
+
+        //Recovered
+        let currentRecovered = JSON.parse(JSON.stringify(dataCopy[day].recovered)) 
+        dataCopy[day].recovered = currentRecovered - lastRecovered
+        lastRecovered = currentRecovered
+
+        //Deaths
+        let currentDeaths = JSON.parse(JSON.stringify(dataCopy[day].deaths)) 
+        dataCopy[day].deaths = currentDeaths - lastDeaths
+        lastDeaths = currentDeaths
       }
       setDailyData(dataCopy)
     }
@@ -32,7 +82,7 @@ const GlobalDaily = ({data}) => {
 
   return(
     <div className="chart-container global-daily">
-      <h2 className="chart-title">Global cumulative confirmed cases</h2>
+      <h2 className="chart-title">Global daily cases</h2>
       {dailyData && 
         <ResponsiveContainer width='100%' height={400}>
           <LineChart
@@ -44,10 +94,29 @@ const GlobalDaily = ({data}) => {
           <XAxis dataKey="date" tick={{ angle: -15 }} dy={8} height={50} />
           <YAxis />
           <Tooltip content={<CustomTooltip />} />
-          <Line type="monotone" dataKey="confirmed" dot={{ stroke: 'none', fill: '#5078AA', r: 2 }}/>
+          {displayConfirmed && <Line type="monotone" dataKey="confirmed" stroke="#5078AA" dot={{ stroke: 'none', fill: '#5078AA', r: 2 }}/>}
+          {displayRecovered && <Line type="monotone" dataKey="recovered" stroke="#74FFA7" dot={{ stroke: 'none', fill: '#74FFA7', r: 2 }}/>}
+          {displayDeaths && <Line type="monotone" dataKey="deaths" stroke="#FF6060" dot={{ stroke: 'none', fill: '#FF6060', r: 2 }}/>}
           </LineChart>
         </ResponsiveContainer>
       }
+      <div className="checkbox-container">
+        <FormControlLabel
+          value="confirmed"
+          control={<ConfirmedCheckbox checked={displayConfirmed} onChange={() => setDisplayConfirmed(!displayConfirmed)}/>}
+          label="Confirmed"
+        />
+         <FormControlLabel
+          value="recovered"
+          control={<RecoveredCheckbox checked={displayRecovered} onChange={() => setDisplayRecovered(!displayRecovered)}/>}
+          label="Recovered"
+        />
+         <FormControlLabel
+          value="deaths"
+          control={<DeathsCheckBox checked={displayDeaths} onChange={() => setDisplayDeaths(!displayDeaths)}/>}
+          label="Deaths"
+        />
+      </div>
     </div>
   )
 }
--- src/Components/Charts/GlobalTotal/GlobalTotal.js
+++ src/Components/Charts/GlobalTotal/GlobalTotal.js
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+import { withStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const ConfirmedCheckbox = withStyles({
+  root: {
+    color: '#5078AA',
+    '&$checked': {
+      color: '#5078AA',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+const RecoveredCheckbox = withStyles({
+  root: {
+    color: '#74FFA7',
+    '&$checked': {
+      color: '#74FFA7',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+
+const DeathsCheckBox = withStyles({
+  root: {
+    color: '#FF6060',
+    '&$checked': {
+      color: '#FF6060',
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
 
 const GlobalTotal = ({data}) => {
+  const [displayConfirmed, setDisplayConfirmed] = useState(true)
+  const [displayRecovered, setDisplayRecovered] = useState(true)
+  const [displayDeaths, setDisplayDeaths] = useState(true)
+
   const CustomTooltip = ({ payload }) => {
     return (
       <div className="tooltip">
         <b>{payload?.[0]?.payload?.date}</b>
         <span>
-          <p>
-           Confirmed: {payload?.[0]?.payload?.confirmed.toLocaleString()}
-          </p>
-          <p>
-           Recovered: {payload?.[0]?.payload?.recovered.toLocaleString()}
-          </p>
-          <p>
-           Deaths: {payload?.[0]?.payload?.deaths.toLocaleString()}
-          </p>
+          {displayConfirmed && <p>Confirmed: {payload?.[0]?.payload?.confirmed.toLocaleString()}</p>}
+          {displayRecovered && <p>Recovered: {payload?.[0]?.payload?.recovered.toLocaleString()}</p>}
+          {displayDeaths && <p>Deaths: {payload?.[0]?.payload?.deaths.toLocaleString()}</p>}
         </span>
       </div>
     )
@@ -23,7 +55,7 @@ const GlobalTotal = ({data}) => {
 
   return(
     <div className="chart-container global-total">
-      <h2 className="chart-title">Global Total</h2>
+      <h2 className="chart-title">Global cumulative cases</h2>
       {data && 
         <ResponsiveContainer width='100%' height={400}>
           <AreaChart
@@ -36,13 +68,30 @@ const GlobalTotal = ({data}) => {
           <XAxis dataKey="date" tick={{ angle: -15 }} dy={8} height={50} />
           <YAxis />
           <Tooltip content={<CustomTooltip />} />
-          <Legend />
-          <Area type="monotone" dataKey="confirmed" stackId="1" stroke="#6EADFF" fill="#6EADFF"/>
-          <Area type="monotone" dataKey="recovered" stackId="2" stroke="#74FFA7" fill="#74FFA7"/>
-          <Area type="monotone" dataKey="deaths" stackId="3" stroke="#FF6060" fill="#FF6060"/>
+          {/* <Legend /> */}
+          {displayConfirmed && <Area type="monotone" dataKey="confirmed" stackId="1" stroke="#6EADFF" fill="#6EADFF"/>}
+          {displayRecovered && <Area type="monotone" dataKey="recovered" stackId="2" stroke="#74FFA7" fill="#74FFA7"/>}
+          {displayDeaths && <Area type="monotone" dataKey="deaths" stackId="3" stroke="#FF6060" fill="#FF6060"/>}
           </AreaChart>
         </ResponsiveContainer>
       }
+       <div className="checkbox-container">
+        <FormControlLabel
+          value="confirmed"
+          control={<ConfirmedCheckbox checked={displayConfirmed} onChange={() => setDisplayConfirmed(!displayConfirmed)}/>}
+          label="Confirmed"
+        />
+         <FormControlLabel
+          value="recovered"
+          control={<RecoveredCheckbox checked={displayRecovered} onChange={() => setDisplayRecovered(!displayRecovered)}/>}
+          label="Recovered"
+        />
+         <FormControlLabel
+          value="deaths"
+          control={<DeathsCheckBox checked={displayDeaths} onChange={() => setDisplayDeaths(!displayDeaths)}/>}
+          label="Deaths"
+        />
+      </div>
     </div>
   )
 }
